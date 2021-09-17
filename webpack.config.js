4a291e47c4d8ed1ecd72f99ee869fa52cb285b35
Dmitriy Sopp <sp_dmitriy@mail.ru>
2021-09-17 13:36:21 +0800

    Finish project config

---
+++ webpack.config.js
@@ -0,0 +1,85 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const CopyPlugin = require('copy-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const ESLintPlugin = require('eslint-webpack-plugin')
+
+module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production'
+  const isDev = !isProd
+
+  const filename = (ext) =>
+    isProd ? `[name].[contenthash].bundle.${ext}` : `[name].bundle.${ext}`
+
+  const plugins = () => {
+    const basePlugins = [
+      new HtmlWebpackPlugin({
+        title: 'JS-EXCEL_1.0.0',
+        template: './index.html'
+      }),
+      new CopyPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, 'src', 'favicon.ico'),
+            to: path.resolve(__dirname, 'dist')
+          },
+        ],
+      }),
+      new MiniCssExtractPlugin({
+        filename: filename('css')
+      })
+    ]
+
+    if (isDev) {
+      basePlugins.push(new ESLintPlugin())
+    }
+
+    return basePlugins
+  }
+
+  return {
+    target: 'web',
+    context: path.resolve(__dirname, 'src'),
+    entry: {
+      main: ['core-js/stable', 'regenerator-runtime/runtime', './index.js'],
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: filename('js'),
+      clean: true,
+    },
+    resolve: {
+      extensions: ['.js'],
+      alias: {
+        '@': path.resolve(__dirname, 'src'),
+        '@core': path.resolve(__dirname, 'src', 'core'),
+      },
+    },
+    devServer: {
+      port: 3000,
+      open: true,
+      hot: true,
+      watchFiles: './',
+    },
+    devtool: isDev ? 'source-map' : false,
+    plugins: plugins(),
+    module: {
+      rules: [
+        {
+          test: /\.s[ac]ss$/i,
+          use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
+      ],
+    },
+  };
+};
